469586b4492062ff165d5c40d8315b273d2ce5a2
Maze7 <marcel-maze7@hotmail.de>
2019-04-02 23:02:16 +0200

    whitespaces typo

--- src/main/java/net/biocrafting/groupbot/databases/ConnectionException.java
+++ src/main/java/net/biocrafting/groupbot/databases/ConnectionException.java
@@ -9,7 +9,7 @@ public class ConnectionException extends Exception {
 	public ConnectionException(String message) {
 		super(message);
 	}
-    public ConnectionException(String message, Throwable cause) {
-        super(message, cause);
-    }
+	public ConnectionException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
