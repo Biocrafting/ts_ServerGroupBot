37741f1f995f3ae1cba5f21fdbfa16836032d2a7
Maze7 <marcel-maze7@hotmail.de>
2019-04-02 22:57:51 +0200

    dont execute project when settings arent set

--- src/main/java/net/biocrafting/groupbot/ServerGroupBot.java
+++ src/main/java/net/biocrafting/groupbot/ServerGroupBot.java
@@ -10,21 +10,22 @@ public class ServerGroupBot {
 				|| Configuration.PROPERTY_DB_SERVER.isEmpty()
 				|| Configuration.PROPERTY_DB_USER.isEmpty()) {			
 			System.out.println("Please set all database configuration in Configuration class");
-		}
-		try {
-			GroupBot bot = new GroupBot(args);
-			boolean success = bot.extractAll();
-			if (success) {
-				bot.setGroup();
-			} else {
-				System.out.println("There was an error during user extraction from database");
-			}
-		} catch (IllegalArgumentException e) {
-			System.out.println(e.getMessage());
-		} catch (Exception e) {
-			System.out.println("Errrrrrrr");
-			if (Configuration.isDebugEnable()) {
-				e.printStackTrace();
+		} else {
+			try {
+				GroupBot bot = new GroupBot(args);
+				boolean success = bot.extractAll();
+				if (success) {
+					bot.setGroup();
+				} else {
+					System.out.println("There was an error during user extraction from database");
+				}
+			} catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+			} catch (Exception e) {
+				System.out.println("Errrrrrrr");
+				if (Configuration.isDebugEnable()) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
